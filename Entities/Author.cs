ccfdd692f858fc5e12d4768807b1ef5387ec5fb8
Jorge Aguilera <giito16@gmail.com>
2023-09-13 22:45:36 -0600

    :art: Added: Validations attributes

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -1,9 +1,21 @@
-﻿namespace WebApiAutores.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validators;
+
+namespace WebApiAutores.Entities
 {
     public class Author
     {
         public int id { get; set; }
+
+        [Required(ErrorMessage ="Field {0} is required")]
+        [StringLength(maximumLength:5, ErrorMessage = "Length should not exceed {1}")]
+        [TitleCase]
         public string name{ get; set; }
+
+        [Range(18,100)]
+        [NotMapped]
+        public int age { get; set; }
 
         public List<Book> Books { get; set; }
     }
